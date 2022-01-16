87e61a8e8b7c663674f38e2ae8f1a3857dd0ce67
shun340 <92897220+shun340@users.noreply.github.com>
2022-01-16 23:51:33 +0000

    [Add]Install Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #管理者
+  devise_for :admin, skip: [:passwords], controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+  
+  #妊婦・患者
+  devise_for :patients, skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
 end
