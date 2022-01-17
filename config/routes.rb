78891164234d0d131000dc12915616ce0785fdff
shun340 <92897220+shun340@users.noreply.github.com>
2022-01-17 08:02:59 +0000

    [Add]Checks Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   
-  #管理者
+  #管理者側
   devise_for :admin, skip: [:passwords], controllers: {
     registrations: "admin/registrations",
     sessions: "admin/sessions"
   }
   
-  #妊婦・患者
+  #妊婦・患者側
   devise_for :patients, skip: [:passwords],controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
