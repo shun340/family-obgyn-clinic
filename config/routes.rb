e8e932bcdba24ab0fd877d1236c72531d9b88265
shun340 <92897220+shun340@users.noreply.github.com>
2022-01-18 09:53:31 +0000

    [Add]Set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,42 @@
 Rails.application.routes.draw do
-  
+
+
   #管理者側
   devise_for :admin, skip: [:passwords], controllers: {
     registrations: "admin/registrations",
     sessions: "admin/sessions"
   }
-  
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :patients, except:[:new, :create, :destroy]
+    resources :appointment_frames, only: :index
+    resources :appointments, only:[:index, :show, :update] do
+      resources :doctor_comments, except:[:index, :show]
+      resource  :checks, only:[:create, :destroy]
+    end
+  end
+
+
   #妊婦・患者側
   devise_for :patients, skip: [:passwords],controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
 
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    
+    resource :patients, only:[:edit, :update] do
+      get "my_page" => "patients#show"
+    end
+    
+    resources :appointment_frames, only:[:index, :edit, :update]
+    resources :appointments, only:[:new, :index, :show] do
+      post "confirm" => "appointments#confirm"
+    end
+  end
+    
+    
 end
