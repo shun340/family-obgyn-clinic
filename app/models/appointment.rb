f3a14b281453e1b23b8a9c8241f47b570ffd66a5
shun340 <92897220+shun340@users.noreply.github.com>
2022-01-19 02:54:43 +0000

    [Add]Set Enum

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,10 +1,15 @@
 class Appointment < ApplicationRecord
   
+  
   # アソシエーション
   has_one  :appointment_frame, dependent: :destroy
   has_many :doctor_comments, dependent: :destroy
   has_many :checks, dependent: :destroy
   
   belongs_to :patient
+  
+  #enum設定
+  ##診察種別
+  enum examination_type: { first_visit: 0, revisit:1 }
   
 end
--- app/models/appointment_frame.rb
+++ app/models/appointment_frame.rb
@@ -1,6 +1,10 @@
 class AppointmentFrame < ApplicationRecord
-  
+
   # アソシエーション
   belongs_to :appointment
-  
+
+  #enum設定
+  ##予約枠ステータス
+  enum app_frame_status: { temporary_hold: 0, reserved: 1, cancel: 2 }
+
 end
